0444a197297cbed28a178362abe3306e71fce133
Duncan Alexander <hypo@hypothete.com>
2019-01-23 08:02:29 -0800

    cleaned up sample code

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -81,20 +81,7 @@ class Canvas extends Component {
     });
   }
 
-//   void plotLine(int x0, int y0, int x1, int y1)
-// {
-//    int dx =  abs(x1-x0), sx = x0<x1 ? 1 : -1;
-//    int dy = -abs(y1-y0), sy = y0<y1 ? 1 : -1; 
-//    int err = dx+dy, e2; /* error value e_xy */
- 
-//    for(;;){  /* loop */
-//       setPixel(x0,y0);
-//       if (x0==x1 && y0==y1) break;
-//       e2 = 2*err;
-//       if (e2 >= dy) { err += dy; x0 += sx; } /* e_xy+e_x > 0 */
-//       if (e2 <= dx) { err += dx; y0 += sy; } /* e_xy+e_y < 0 */
-//    }
-// }
+  // based on http://members.chello.at/~easyfilter/bresenham.html
   bresenham(x0, y0, x1, y1, col) {
     const dx = Math.abs(x1 - x0);
     const dy = -Math.abs(y1 - y0);
