18bdd33ca04b40b20fafdf0052f928e328451390
Duncan Alexander <hypo@hypothete.com>
2019-01-27 15:18:29 -0800

    Updates

---
+++ precache-manifest.4a5bddb39f4bd2387f25037def2563cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94f1d3d642041fd4ca57",
+    "url": "/gmagic/static/css/main.d88cec03.chunk.css"
+  },
+  {
+    "revision": "94f1d3d642041fd4ca57",
+    "url": "/gmagic/static/js/main.94f1d3d6.chunk.js"
+  },
+  {
+    "revision": "0342d4d2e4db1cb341b6",
+    "url": "/gmagic/static/js/1.0342d4d2.chunk.js"
+  },
+  {
+    "revision": "3f52ad7ec957ceb5e567",
+    "url": "/gmagic/static/js/runtime~main.3f52ad7e.js"
+  },
+  {
+    "revision": "8ede147328982c1f280f4fc75c9d8b90",
+    "url": "/gmagic/index.html"
+  }
+];
