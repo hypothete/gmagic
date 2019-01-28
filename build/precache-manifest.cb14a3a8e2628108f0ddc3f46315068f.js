076829e75dfb186c535ab6d45b7b2b55126babf6
Duncan Alexander <hypo@hypothete.com>
2019-01-27 17:07:04 -0800

    more build

---
+++ build/precache-manifest.cb14a3a8e2628108f0ddc3f46315068f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d41177d1285823b496af",
+    "url": "/gmagic/static/css/main.d88cec03.chunk.css"
+  },
+  {
+    "revision": "d41177d1285823b496af",
+    "url": "/gmagic/static/js/main.d41177d1.chunk.js"
+  },
+  {
+    "revision": "0342d4d2e4db1cb341b6",
+    "url": "/gmagic/static/js/1.0342d4d2.chunk.js"
+  },
+  {
+    "revision": "3f52ad7ec957ceb5e567",
+    "url": "/gmagic/static/js/runtime~main.3f52ad7e.js"
+  },
+  {
+    "revision": "e3fcb380c1975da5533089299ec6e1f0",
+    "url": "/gmagic/index.html"
+  }
+];
