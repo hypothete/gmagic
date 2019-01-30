cfc690745a87b2e7e8b282c8157585acfe356cb9
Duncan Alexander <hypo@hypothete.com>
2019-01-30 07:43:40 -0800

    missed build ifles

---
+++ build/precache-manifest.56216fcd0d7b068c691becf306e7e55f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56addcf04c3f0a2e0753",
+    "url": "/gmagic/static/css/main.d88cec03.chunk.css"
+  },
+  {
+    "revision": "56addcf04c3f0a2e0753",
+    "url": "/gmagic/static/js/main.56addcf0.chunk.js"
+  },
+  {
+    "revision": "0342d4d2e4db1cb341b6",
+    "url": "/gmagic/static/js/1.0342d4d2.chunk.js"
+  },
+  {
+    "revision": "3f52ad7ec957ceb5e567",
+    "url": "/gmagic/static/js/runtime~main.3f52ad7e.js"
+  },
+  {
+    "revision": "97d6bde24f11a729ba0e2748c90c55ae",
+    "url": "/gmagic/index.html"
+  }
+];
